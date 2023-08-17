09321404eabd9d3690de1cbfbdb7480df192cd73
“Saphron” <saphron.hastie@gmail.com>
2023-08-17 18:37:38 +1000

    Generate grids

--- etch.js
+++ etch.js
@@ -1,14 +1,29 @@
 "use strict";
 let target = 16;
 const app = document.getElementById("app");
-function createGrid() {
+const cssRoot = document.querySelector(':root');
+function updateCSS(target) {
+    let sqrTarget = Math.sqrt(target);
+    cssRoot === null || cssRoot === void 0 ? void 0 : cssRoot.style.setProperty('--total', sqrTarget);
+    console.log(target);
+}
+function clearGrid() {
+    const grid = document.getElementById("grid");
+    grid === null || grid === void 0 ? void 0 : grid.remove();
+    console.log("goodbye grid!");
+}
+function createGrid(target) {
+    clearGrid();
+    let newGrid = document.createElement('div');
+    newGrid.id = "grid";
+    app === null || app === void 0 ? void 0 : app.appendChild(newGrid);
+    const grid = document.getElementById("grid");
     let i = 0;
+    updateCSS(target);
     while (i++ < target) {
         let newSquare = document.createElement('div');
         newSquare.id = "appSquare";
-        app === null || app === void 0 ? void 0 : app.appendChild(newSquare);
-        // i ++;
+        grid === null || grid === void 0 ? void 0 : grid.appendChild(newSquare);
         console.log(i);
     }
 }
-createGrid();
--- etch.ts
+++ etch.ts
@@ -1,15 +1,31 @@
-let target = 16
+let target: number = 16
 const app = document.getElementById("app");
-const cssRoot = document.querySelector(':root');
+const cssRoot = document.querySelector(':root') as any;
 
-function createGrid(): void {
+function updateCSS(target: number) {
+    let sqrTarget = Math.sqrt(target as number);
+    cssRoot?.style.setProperty('--total', sqrTarget);
+    console.log(target);
+}
+
+function clearGrid(): void {
+    const grid = document.getElementById("grid");
+    grid?.remove();
+    console.log("goodbye grid!")
+}
+
+function createGrid(target: number): void {
+    clearGrid();
+    let newGrid = document.createElement('div');
+    newGrid.id = "grid";
+    app?.appendChild(newGrid);
+    const grid = document.getElementById("grid");
     let i = 0
+    updateCSS(target as number);
     while (i++ < target) {
         let newSquare = document.createElement('div');
         newSquare.id = "appSquare";
-        app?.appendChild(newSquare);
+        grid?.appendChild(newSquare);
         console.log(i);
     }
 }
-
-createGrid();
